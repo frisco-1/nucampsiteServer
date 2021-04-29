7e08447f1a1d6832eeb87430af5a2eb8411fe0f4
Francisco Guerrero <paquito1214.2017@gmail.com>
2021-04-29 16:13:17 -0400

    Using OAuth with passport and Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
   'secretKey': '12345-67890-09876-54321',
-  'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+  'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+  'facebook': {
+    clientId: '2618713001768561',
+    clientSecret: '8078093ca2b0dbf81ec8d763c1401bf1'
+  }
 }
